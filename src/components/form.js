09bf2c3da8e114057217704b74744ce3cff205ed
KonstantinTheRed <61480260+KonstantinTheRed@users.noreply.github.com>
2020-10-09 21:49:50 -0700

    initial push

--- src/components/form.js
+++ src/components/form.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Peer from "peerjs";
 let peer = new Peer();
-let isLiveG = null;
 const initSignal = () => {
   console.log("Awaiting Hash", window.location.hash);
   peer.on("open", function (id) {
